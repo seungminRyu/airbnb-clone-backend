cfcfd5a4b428ffc0ada8b4b1deaa3cb3e1aaabac
seungiminRyu <fbtmdals011@naver.com>
2025-07-23 16:02:21 +0900

    #4.4 AbstractItem 상속 모델들 생성, on_delete

--- rooms/models.py
+++ rooms/models.py
@@ -17,6 +17,26 @@ class AbstractItem(core_models.TimeStampedModel):
 
 
 class RoomType(AbstractItem):
+    """RoomType Model Definition"""
+
+    pass
+
+
+class Amenity(AbstractItem):
+    """Amenity Model definition"""
+
+    pass
+
+
+class Facility(AbstractItem):
+    """Facility Model definition"""
+
+    pass
+
+
+class HouseRule(AbstractItem):
+    """HouseRule Model definition"""
+
     pass
 
 
@@ -37,7 +57,10 @@ class Room(core_models.TimeStampedModel):
     check_out = models.TimeField()
     instant_book = models.BooleanField(default=False)
     host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
-    room_type = models.ManyToManyField(RoomType, blank=True)
+    room_type = models.ForeignKey(RoomType, on_delete=models.SET_NULL, null=True)
+    amenities = models.ManyToManyField(Amenity)
+    facilities = models.ManyToManyField(Facility)
+    house_rules = models.ManyToManyField(HouseRule)
 
     def __str__(self):
         return self.name
