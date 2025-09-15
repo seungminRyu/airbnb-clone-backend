05411961bf3ab4286f0a9aef939164121f0d2506
seungiminRyu <fbtmdals011@naver.com>
2025-09-15 15:47:55 +0900

    #10.3 카테고리 시리얼라이저 추가

--- categories/views.py
+++ categories/views.py
@@ -1,8 +1,11 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Category
+from .serializers import CategorySerializer
 
 
 @api_view()
 def categories(request):
-    return Response({"ok": True, "categories": list(Category.objects.all().values())})
+    all_categories = Category.objects.all()
+    serializer = CategorySerializer(all_categories, many=True)
+    return Response({"ok": True, "categories": serializer.data})
