aad1efb7cac92ed0a626b08172cbdd5c584d0096
seungiminRyu <fbtmdals011@naver.com>
2025-09-15 16:22:39 +0900

    #10.4 카테고리 post 요청, 개별 get 요청 추가

--- categories/views.py
+++ categories/views.py
@@ -4,8 +4,27 @@ from .models import Category
 from .serializers import CategorySerializer
 
 
-@api_view()
+@api_view(["GET", "POST"])
 def categories(request):
-    all_categories = Category.objects.all()
-    serializer = CategorySerializer(all_categories, many=True)
-    return Response({"ok": True, "categories": serializer.data})
+    if request.method == "GET":
+        all_categories = Category.objects.all()
+        serializer = CategorySerializer(all_categories, many=True)
+        return Response({"ok": True, "categories": serializer.data})
+
+    elif request.method == "POST":
+        kind = request.data.get("kind")
+        name = request.data.get("name")
+        Category.objects.create(kind=kind, name=name)
+        return Response(
+            {
+                "created": True,
+            }
+        )
+
+
+@api_view(["GET"])
+def category(request, pk):
+    if request.method == "GET":
+        category = Category.objects.get(pk=pk)
+        serializer = CategorySerializer(category)
+        return Response({"ok": True, "category": serializer.data})
