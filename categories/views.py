31382d1ca92307ed2ec150f7331bb8b1922c4066
seungiminRyu <fbtmdals011@naver.com>
2025-09-17 10:44:10 +0900

    #10.7 시리얼라이저 update 추가

--- categories/views.py
+++ categories/views.py
@@ -1,5 +1,6 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.exceptions import NotFound
 from .models import Category
 from .serializers import CategorySerializer
 
@@ -7,22 +8,51 @@ from .serializers import CategorySerializer
 @api_view(["GET", "POST"])
 def categories(request):
     if request.method == "GET":
-        all_categories = Category.objects.all()
-        serializer = CategorySerializer(all_categories, many=True)
-        return Response({"ok": True, "categories": serializer.data})
+        return get_all_categories()
 
     elif request.method == "POST":
-        serializer = CategorySerializer(data=request.data)
-        if serializer.is_valid():
-            new_category = serializer.save()
-            return Response(CategorySerializer(new_category).data)
-        else:
-            return Response(serializer.errors)
+        return create_category(request.data)
 
 
-@api_view(["GET"])
+@api_view(["GET", "PUT"])
 def category(request, pk):
+    category = get_category(pk)
+
     if request.method == "GET":
-        category = Category.objects.get(pk=pk)
         serializer = CategorySerializer(category)
-        return Response({"ok": True, "category": serializer.data})
+        return Response(serializer.data)
+    elif request.method == "PUT":
+        return update_category(pk, request.data)
+
+
+def get_all_categories():
+    all_categories = Category.objects.all()
+    serializer = CategorySerializer(all_categories, many=True)
+    return Response({"ok": True, "categories": serializer.data})
+
+
+def create_category(data):
+    serializer = CategorySerializer(data=data)
+    if serializer.is_valid():
+        new_category = serializer.save()
+        return Response(CategorySerializer(new_category).data)
+    else:
+        return Response(serializer.errors)
+
+
+def get_category(pk):
+    try:
+        category = Category.objects.get(pk=pk)
+        return category
+    except Category.DoesNotExist:
+        raise NotFound
+
+
+def update_category(pk, data):
+    category = get_category(pk)
+    serializer = CategorySerializer(category, data=data, partial=True)
+    if serializer.is_valid():
+        updated_category = serializer.save()
+        return Response(CategorySerializer(updated_category).data)
+    else:
+        return Response(serializer.errors)
