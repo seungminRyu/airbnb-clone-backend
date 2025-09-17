577e8e2af4fa6e32fab3b8e0db4332c11ba0e4f3
seungiminRyu <fbtmdals011@naver.com>
2025-09-17 13:56:32 +0900

    #10.11 모델 시리얼라이저로 변경

--- categories/serializers.py
+++ categories/serializers.py
@@ -2,17 +2,8 @@ from rest_framework import serializers
 from .models import Category
 
 
-class CategorySerializer(serializers.Serializer):
-    pk = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(max_length=50, required=True)
-    kind = serializers.ChoiceField(choices=Category.CategoryKindChoices.choices)
-    created_at = serializers.DateTimeField(read_only=True)
+class CategorySerializer(serializers.ModelSerializer):
 
-    def create(self, validated_data):
-        return Category.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get("name", instance.name)
-        instance.kind = validated_data.get("kind", instance.kind)
-        instance.save()
-        return instance
+    class Meta:
+        model = Category
+        exclude = ["updated_at"]
