305eb006e57a2d271396f7b73e54736917f14349
seungiminRyu <fbtmdals011@naver.com>
2025-09-17 16:19:47 +0900

    #11.1 amenity detail 뷰 구현

--- rooms/views.py
+++ rooms/views.py
@@ -1,5 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.exceptions import NotFound
+from rest_framework.status import HTTP_204_NO_CONTENT
+
 from .models import Amenity
 from .serializers import AmenitySerializer
 
@@ -22,11 +25,28 @@ class Amenities(APIView):
 
 
 class AmenityDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Amenity.objects.get(pk=pk)
+        except Amenity.DoesNotExist:
+            return NotFound
+
     def get(self, request, pk):
-        pass
+        serializer = AmenitySerializer(self.get_object(pk))
+        return Response(serializer.data)
 
     def put(self, request, pk):
-        pass
+        serializer = AmenitySerializer(
+            self.get_object(pk), data=request.data, partial=True
+        )
+
+        if serializer.is_valid():
+            updated_amenity = serializer.save()
+            return Response(AmenitySerializer(updated_amenity).data)
+        else:
+            return Response(serializer.errors)
 
     def delete(self, request, pk):
-        pass
+        amenity = self.get_object(pk)
+        amenity.delete()
+        return Response(status=HTTP_204_NO_CONTENT)
