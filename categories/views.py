c02f0c1b57dd37bcd5ca9bead0c08ff521a15c5a
seungiminRyu <fbtmdals011@naver.com>
2025-09-17 11:38:55 +0900

    #10.8 카테고리 delete 추가

--- categories/views.py
+++ categories/views.py
@@ -1,6 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.exceptions import NotFound
+from rest_framework.status import HTTP_204_NO_CONTENT
 from .models import Category
 from .serializers import CategorySerializer
 
@@ -14,7 +15,7 @@ def categories(request):
         return create_category(request.data)
 
 
-@api_view(["GET", "PUT"])
+@api_view(["GET", "PUT", "DELETE"])
 def category(request, pk):
     category = get_category(pk)
 
@@ -23,6 +24,9 @@ def category(request, pk):
         return Response(serializer.data)
     elif request.method == "PUT":
         return update_category(pk, request.data)
+    elif request.method == "DELETE":
+        category.delete()
+        return Response(status=HTTP_204_NO_CONTENT)
 
 
 def get_all_categories():
