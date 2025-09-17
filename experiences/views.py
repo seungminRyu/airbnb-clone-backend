4c9efd1554ea0a8f92a614b9b7a1115528dba132
seungiminRyu <fbtmdals011@naver.com>
2025-09-17 16:51:29 +0900

    #11.2 perk detail, perks 뷰 구현

--- experiences/views.py
+++ experiences/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.status import HTTP_204_NO_CONTENT
+from rest_framework.exceptions import NotFound
 
-# Create your views here.
+from .models import Perk
+from .serializers import PerkSerializer
+
+
+class Perks(APIView):
+    def get(self, request):
+        all_perks = Perk.objects.all()
+        serializer = PerkSerializer(all_perks, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = PerkSerializer(data=request.data)
+        if serializer.is_valid():
+            new_perk = serializer.save()
+            return Response(PerkSerializer(new_perk).data)
+        else:
+            return Response(PerkSerializer.errors)
+
+
+class PerkDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Perk.objects.get(pk=pk)
+        except Perk.DoesNoExist:
+            return NotFound
+
+    def get(self, request, pk):
+        perk = self.get_object(pk)
+        return Response(PerkSerializer(perk).data)
+
+    def put(self, request, pk):
+        perk = self.get_object(pk)
+        serializer = PerkSerializer(perk, data=request.data, partial=True)
+
+        if serializer.is_valid():
+            updated_perk = serializer.save()
+            return Response(PerkSerializer(updated_perk).data)
+        else:
+            return Response(PerkSerializer.errors)
+
+    def delete(self, request, pk):
+        perk = self.get_object(pk)
+        perk.delete()
+        return Response(status=HTTP_204_NO_CONTENT)
