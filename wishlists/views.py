eb146ba31cf354b81dcf2792b2ead687f20b083e
seungiminRyu <fbtmdals011@naver.com>
2025-09-29 17:13:37 +0900

    #11.20 위시리스트 시리얼라이저 디테일, 토글 추가

--- wishlists/views.py
+++ wishlists/views.py
@@ -1,8 +1,11 @@
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.exceptions import NotFound
+from rest_framework.status import HTTP_200_OK
 from .models import Wishlist
 from .serializers import WishlistSerializer
+from rooms.models import Room
 
 
 class Wishlists(APIView):
@@ -27,3 +30,68 @@ class Wishlists(APIView):
             return Response(new_serializer.data)
         else:
             return Response(serializer.errors)
+
+
+class WishlistDetail(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self, pk, user):
+        try:
+            return Wishlist.objects.get(user=user, pk=pk)
+        except Wishlist.DoesNotExist:
+            raise NotFound
+
+    def get(self, request, pk):
+        wishlist = self.get_object(pk=pk, user=request.user)
+        serializer = WishlistSerializer(
+            wishlist,
+            context={"request": request},
+        )
+        return Response(serializer.data)
+
+    def delete(self, request, pk):
+        wishlist = self.get_object(pk=pk, user=request.user)
+        wishlist.delete()
+        return Response(status=HTTP_200_OK)
+
+    def put(self, request, pk):
+        wishlist = self.get_object(pk=pk, user=request.user)
+        serializer = WishlistSerializer(
+            wishlist,
+            data=request.data,
+            partial=True,
+        )
+
+        if serializer.is_valid():
+            updated_wishlist = serializer.save()
+            updated_serializer = WishlistSerializer(
+                updated_wishlist,
+                context={"request": request},
+            )
+            return Response(updated_serializer.data)
+        else:
+            return Response(serializer.errors)
+
+
+class WishlistToggle(APIView):
+    def get_list(self, pk, user):
+        try:
+            return Wishlist.object.get(pk=pk, user=user)
+        except Wishlist.DoesNotExist:
+            raise NotFound
+
+    def get_room(self, pk):
+        try:
+            return Room.object.get(pk=pk)
+        except Room.DoesNotExist:
+            raise NotFound
+
+    def put(self, request, pk, room_pk):
+        wishlist = self.get_list(pk=pk, user=request.user)
+        room = self.get_room(pk=room_pk)
+
+        if wishlist.rooms.filter(pk=room_pk).exists():
+            wishlist.remove(room)
+        else:
+            wishlist.add(room)
+        return Response(HTTP_200_OK)
