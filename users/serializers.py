c8a5e2cc4069077af07c30f961ec8ff814c2d7f6
seungiminRyu <fbtmdals011@naver.com>
2025-10-03 21:55:48 +0900

    #12.0 유저 프로필 api 추가

--- users/serializers.py
+++ users/serializers.py
@@ -6,3 +6,19 @@ class TinyUserSerializer(ModelSerializer):
     class Meta:
         model = User
         fields = ("name", "avatar", "username")
+
+
+class PrivateUserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        exclude = [
+            "password",
+            "is_active",
+            "groups",
+            "first_name",
+            "last_name",
+            "is_staff",
+            "is_superuser",
+            "id",
+            "user_permissions",
+        ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
 
-# Create your views here.
+from users.serializers import PrivateUserSerializer
+
+
+class Me(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        serializer = PrivateUserSerializer(request.user)
+        return Response(serializer.data)
+
+    def put(self, request):
+        serializer = PrivateUserSerializer(
+            request.user,
+            data=request.data,
+            partial=True,
+        )
+        if serializer.is_valid():
+            updated_user = serializer.save()
+            Response(PrivateUserSerializer(updated_user).data, status=HTTP_200_OK)
+        else:
+            Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
