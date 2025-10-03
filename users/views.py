c94cf6e2063860a4553ee602cc813ef18ebe0b3f
seungiminRyu <fbtmdals011@naver.com>
2025-10-03 22:11:36 +0900

    #12.1 유저생성 추가

--- users/views.py
+++ users/views.py
@@ -1,9 +1,31 @@
+from rest_framework.exceptions import ParseError
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
 
 from users.serializers import PrivateUserSerializer
+
+
+class Users(APIView):
+    def post(self, request):
+        password = request.data.get("password")
+        if not password:
+            raise ParseError
+
+        serializer = PrivateUserSerializer(data=request.data)
+        if serializer.is_valid():
+            new_user = serializer.save()
+            new_user.set_password(password)
+            Response(
+                PrivateUserSerializer(new_user).data,
+                status=HTTP_200_OK,
+            )
+        else:
+            Response(
+                serializer.errors,
+                status=HTTP_400_BAD_REQUEST,
+            )
 
 
 class Me(APIView):
@@ -21,6 +43,12 @@ class Me(APIView):
         )
         if serializer.is_valid():
             updated_user = serializer.save()
-            Response(PrivateUserSerializer(updated_user).data, status=HTTP_200_OK)
+            Response(
+                PrivateUserSerializer(updated_user).data,
+                status=HTTP_200_OK,
+            )
         else:
-            Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+            Response(
+                serializer.errors,
+                status=HTTP_400_BAD_REQUEST,
+            )
