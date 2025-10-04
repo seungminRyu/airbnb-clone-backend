9c85c7ded04d9d8ceaea52e368dc430030f03ca5
seungiminRyu <fbtmdals011@naver.com>
2025-10-04 18:14:19 +0900

    #12.2 유저 비밀번호 변경, 퍼블릭 유저 get 추가

--- users/views.py
+++ users/views.py
@@ -1,10 +1,11 @@
-from rest_framework.exceptions import ParseError
+from rest_framework.exceptions import NotFound, ParseError
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
 
-from users.serializers import PrivateUserSerializer
+from .serializers import PrivateUserSerializer
+from .models import User
 
 
 class Users(APIView):
@@ -52,3 +53,33 @@ class Me(APIView):
                 serializer.errors,
                 status=HTTP_400_BAD_REQUEST,
             )
+
+
+class ChangePassword(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def put(self, request):
+        user = request.user
+        old_password = request.data.get("old_password")
+        new_password = request.data.get("new_password")
+
+        if not old_password or not new_password:
+            raise ParseError
+
+        if user.check_password(old_password):
+            user.set_password(new_password)
+            user.save()
+            return Response(status=HTTP_200_OK)
+        else:
+            raise ParseError
+
+
+class PublicUser(APIView):
+    def get(self, request, username):
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            raise NotFound
+
+        serializer = PrivateUserSerializer(user)
+        return Response(serializer.data)
