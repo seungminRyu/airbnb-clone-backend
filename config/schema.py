85af48300651e170862eb81ac4414b78098b9fe0
seungiminRyu <fbtmdals011@naver.com>
2025-10-05 16:18:22 +0900

    #14.2 graph Movie 모델 선언

--- config/schema.py
+++ config/schema.py
@@ -1,15 +1,29 @@
 import strawberry
+import typing
+
+
+@strawberry.type
+class Movie:
+    pk: int
+    title: str
+    year: int
+    rating: int
+
+
+movie_db = [
+    Movie(pk=1, title="Godfather", year=1990, rating=10),
+]
 
 
 @strawberry.type
 class Query:
     @strawberry.field
-    def ping(self) -> str:
-        return "pong"
+    def movies(self) -> typing.List[Movie]:
+        return movie_db
 
     @strawberry.field
-    def hello(self, name: str = "world!") -> str:
-        return f"Hello, {name}"
+    def movie(self, movie_pk: int) -> Movie:
+        return movie_db[movie_pk - 1]
 
 
 schema = strawberry.Schema(query=Query)
