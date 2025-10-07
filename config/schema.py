02cae6bad490d22a9598682f7e44f7a202019823
seungiminRyu <fbtmdals011@naver.com>
2025-10-07 14:41:45 +0900

    #14.5 장고 타입으로 변경

--- config/schema.py
+++ config/schema.py
@@ -1,51 +1,18 @@
 import strawberry
-import typing
+from rooms import schema as rooms_schema
 
 
 @strawberry.type
-class Movie:
-    pk: int
-    title: str
-    year: int
-    rating: int
-
-
-movie_db = [
-    Movie(pk=1, title="Godfather", year=1990, rating=10),
-]
-
-
-def movies() -> typing.List[Movie]:
-    return movie_db
-
-
-def movie(movie_pk: int) -> Movie:
-    return movie_db[movie_pk - 1]
-
-
-@strawberry.type
-class Query:
-    movies: typing.List[Movie] = strawberry.field(resolver=movies)
-    movie: Movie = strawberry.field(resolver=movie)
-
-
-def add_movie(title: str, year: int, rating: int) -> Movie:
-    new_movie = Movie(
-        pk=len(movie_db) + 1,
-        title=title,
-        year=year,
-        rating=rating,
-    )
-    movie_db.append(new_movie)
-    return new_movie
+class Query(rooms_schema.Qeury):
+    pass
 
 
 @strawberry.type
 class Mutation:
-    add_movie: Movie = strawberry.mutation(resolver=add_movie)
+    pass
 
 
 schema = strawberry.Schema(
     query=Query,
-    mutation=Mutation,
+    # mutation=Mutation,
 )
