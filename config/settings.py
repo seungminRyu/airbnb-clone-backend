ac2048fd388d801fd44c64a309323abad4e439a0
seungiminRyu <fbtmdals011@naver.com>
2025-10-23 08:10:26 +0900

    #15.6 env 패키지, env 추가

--- config/settings.py
+++ config/settings.py
@@ -1,14 +1,19 @@
 from pathlib import Path
+import os
+import environ
+
+env = environ.Env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-$-5hs@zadx*n5$6=g!%rn1axe-rvpt(iskyxj-!nh8ku6av58o"
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
+
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
